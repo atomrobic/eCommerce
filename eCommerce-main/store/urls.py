be1319fa7c45b82e80ed58fa99fa5598f85a3f7c
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-04-28 14:26:53 +0530

    erge

--- eCommerce-main/store/urls.py
+++ eCommerce-main/store/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
         path('update_item/', views.updateItem, name="update_item"),
         path('process_order/', views.processOrder, name="process_order"),
         path('process_order/', views.processOrder, name="process_order"),
+        path('process_order/', views.processOrder, name="process_order"),
+        path('process_order/', views.processOrder, name="process_order"),
+
+
 
 
 
