32a1d6098bd0f38b20b6c16b3b55f4c96a00d375
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-04-28 20:12:33 +0530

    all

--- eCommerce-main/store/urls.py
+++ eCommerce-main/store/urls.py
@@ -8,12 +8,15 @@ urlpatterns = [
         path('checkout/', views.checkout, name="checkout"),
         path('update_item/', views.updateItem, name="update_item"),
         path('process_order/', views.processOrder, name="process_order"),
+<<<<<<< HEAD
         path('process_order/', views.processOrder, name="process_order"),
         path('process_order/', views.processOrder, name="process_order"),
         path('process_order/', views.processOrder, name="process_order"),
 
 
 
+=======
+>>>>>>> parent of c140eaf (added)
 
 
 
